0d215331c0e4443a54583841236103b10746d9c9
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-02-11 15:50:07 +0200

    task1

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,16 @@
+const STORAGE_KEY = "items";
 
+export function saveDataForm(value) {
+  const arrItem = getDataPrev();
+  arrItem.push(value);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(arrItem));
+}
+
+export function getDataPrev() {
+  try {
+    const dataJson = localStorage.getItem(STORAGE_KEY);
+    return dataJson ? JSON.parse(dataJson) : [];
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
+import { formEl } from "./refs";
+import { saveDataForm } from "./api";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
+formEl.addEventListener("submit", readDataForm);
+
+function readDataForm(event) {
+  event.preventDefault();
+
+  const value = event.target.message.value.trim();
+  console.log(value);
+
+  if (!value) {
+    return;
+  }
+
+  const data = constructObject(value);
+  saveDataForm(data);
+}
+
+function constructObject(value) {
+  return {
+    value,
+    checked: false,
+    id: Date.now(),
+  };
+}
