b733fd425c0c3e7693bc2a1869bbd61eb7294e45
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-04-28 11:29:53 +0300

    isChecked

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/style.css";
-import { formEl, listEl } from "./refs/";
-import { save, load } from "./api";
-import { createMarkup } from "./markup";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import { formEl, listEl } from './refs/';
+import { save, load } from './api';
+import { createMarkup } from './markup';
 
-const STORAGE_KEY = "message";
+const STORAGE_KEY = 'message';
 
-formEl.addEventListener("submit", getValues);
+formEl.addEventListener('submit', getValues);
+listEl.addEventListener('click', isChecked);
 init();
 
 function getValues(ev) {
@@ -45,19 +46,57 @@ function init() {
 }
 
 function addMarkup(markup) {
-  listEl.insertAdjacentHTML("beforeend", markup);
+  listEl.insertAdjacentHTML('beforeend', markup);
 }
 
-listEl.addEventListener('click', (ev) => {
-    if (ev.target.tagName !== 'BUTTON') {
-        return
+listEl.addEventListener('click', ev => {
+  if (ev.target.tagName !== 'BUTTON') {
+    return;
+  }
+  const parent = ev.target.closest('.item');
+  const taskId = parent.dataset.id;
+  const items = load(STORAGE_KEY);
+  const filterData = items.filter(({ id }) => {
+    return Number(id) !== Number(taskId);
+  });
+  save(STORAGE_KEY, filterData);
+  parent.remove();
+});
+
+function isChecked(event) {
+  if (event.target.tagName !== 'P') {
+    return;
+  }
+
+  if (!event.target.parentNode.classList.contains('checked')) {
+    addChecked(event);
+  } else {
+    disChecked(event);
+  }
+}
+
+function addChecked(ev) {
+  ev.target.parentNode.classList.add('checked');
+  const taskId = Number(ev.target.parentNode.dataset.id);
+  const items = load(STORAGE_KEY);
+  const checkedArr = items.map(el => {
+    if (el.id === taskId) {
+      el.checked = true;
     }
-    const parent = ev.target.closest(".item")
-    const taskId = parent.dataset.id
-    const items = load(STORAGE_KEY)
-    const filterData = items.filter(({ id }) => {
-        return Number(id) !== Number(taskId)
-    })
-    save(STORAGE_KEY, filterData)
-    parent.remove()
-})
+    return el;
+  });
+  save(STORAGE_KEY, checkedArr);
+}
+
+function disChecked(ev) {
+  ev.target.parentNode.classList.remove('checked');
+  const taskId = Number(ev.target.parentNode.dataset.id);
+  const items = load(STORAGE_KEY);
+  const checkedArr = items.map(el => {
+    if (el.id === taskId) {
+      el.checked = false;
+    }
+    return el;
+  });
+  save(STORAGE_KEY, checkedArr);
+}
